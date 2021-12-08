fc5f0f3dc84be83ff98c0e2c4ba045b4049d96d5
joshua <oladokunjoshua2016@gmail.com>
2021-12-08 22:19:54 +0100

    First commit

--- renderer/_default.page.client.ts
+++ renderer/_default.page.client.ts
@@ -1,14 +1,15 @@
-import { getPage } from 'vite-plugin-ssr/client'
-import { createApp } from './app'
-import type { PageContext } from './types'
-import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client'
+import type { PageContextBuiltInClient } from "vite-plugin-ssr/client";
+import { getPage } from "vite-plugin-ssr/client";
+import { createApp } from "./app";
+import "./styles/index.css";
+import type { PageContext } from "./types";
 
-hydrate()
+hydrate();
 
 async function hydrate() {
   // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
   // instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
-  const pageContext = await getPage<PageContextBuiltInClient & PageContext>()
-  const app = createApp(pageContext)
-  app.mount('#app')
+  const pageContext = await getPage<PageContextBuiltInClient & PageContext>();
+  const app = createApp(pageContext);
+  app.mount("#app");
 }
